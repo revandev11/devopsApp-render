7e81a47ee894c8b0b982345dd823e4745465c0f3
Revan <abusovrvan34@gmail.com>
2026-05-11 18:30:27 +0400

    feat:bugs fixed

--- src/main/java/com/example/devopsApp/PersonController.java
+++ src/main/java/com/example/devopsApp/PersonController.java
@@ -8,14 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class PersonController {
-private final PersonService personService;
-
-
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
-    @GetMapping("/hello")
-    public Person helloSayer( @RequestParam String name){
-        return personService.getPerson("hello "+name);
+@GetMapping("/hello")
+    public String helloSayer(){
+        return "Hello, welcome";
     }
 }
